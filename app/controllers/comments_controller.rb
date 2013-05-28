3e92b83d6085ac76418509b5b07ca2d2ac82dbb6
Matthias Brunisholz <hidekin1982@gmail.com>
2013-05-28 14:35:28 +0200

    upload

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find(param[:Post_id])
+    @comment= @post.comments.build(params[:@comment])
+    @comment.save
+
+    redirect_to @post
+
+  end
+
+  def destroy
+   @comment=Comment.find(params[:id])
+    @comment.destroy
+
+    redirect_to @comment.post
+
+
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubytest::Application.config.secret_token = '049cbec109de11d0c4d95d889a615c3145d0332fd4d56f0950cc16ba079e9ab128763f21dafa129f7aa85850052bf1a9bf5b5d0743111c3bda8fe764b0c238c0'
